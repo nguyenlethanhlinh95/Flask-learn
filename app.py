da87a79527939e794ab65a919ee47254275d73de
linhthanh_nitrotech_asia <linhthanh@nitrotech.asia>
2020-02-14 10:39:03 +0700

    Update file app.py and index.html

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
+from xmlrpc.client import Boolean
+
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
-from wtforms.validators import InputRequired, Length, AnyOf
+from wtforms import StringField, PasswordField, IntegerField, BooleanField
+from wtforms.validators import InputRequired, Length, AnyOf, Email
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Mysecret!'
 
 
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[InputRequired('A username is required!'), Length(min=4, max=8,message='Must be between 4,8')])
-    password = PasswordField('password', validators=[InputRequired('A password is required!'), PasswordField])
+    username = StringField('username', validators=[InputRequired('A username is required!'),
+                                                   Length(min=4, max=8, message='Must be between 4,8')])
+    password = PasswordField('password', validators=[InputRequired('A password is required!')])
+    age = IntegerField('age', default=25)
+    true = BooleanField('Click here')
+    email = StringField('email', validators=[Email()])
+
+class User:
+    def __init__(self, username, password, age, email):
+        self.username = username
+        self.password = password
+        self.age = age
+        self.email = email
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
     # gan cho form Class de validator
-    form = LoginForm()
+    myUser = User('Linh', 123, 25, 'nguyenlethanhlinh95@gmail.com')
+    form = LoginForm(obj=myUser)
     if form.validate_on_submit():
-        return '<h1>Username: {} Password: {}</h1>'.format(form.username.data, form.password.data)
+        return '<h1>Username: {} Password: {} Age: {} True: {} Email: {}</h1>'.format(form.username.data,
+                                                                                      form.password.data,
+                                                                                      form.age.data, form.true.data,
+                                                                                      form.email.data)
 
     return render_template('index.html', form=form)
 
