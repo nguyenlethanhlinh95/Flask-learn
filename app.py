6dde9a25da87690a8d2d6b6abf4359fea661dda2
linhthanh_nitrotech_asia <linhthanh@nitrotech.asia>
2020-02-14 09:43:22 +0700

    Update file app for app validator

--- app.py
+++ app.py
@@ -1,12 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, AnyOf
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Mysecret!'
 
 
-@app.route('/')
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired('A username is required!'), Length(min=4, max=8,message='Must be between 4,8')])
+    password = PasswordField('password', validators=[InputRequired('A password is required!'), PasswordField])
+
+
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return 'Hello World!'
+    # gan cho form Class de validator
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>Username: {} Password: {}</h1>'.format(form.username.data, form.password.data)
+
+    return render_template('index.html', form=form)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
